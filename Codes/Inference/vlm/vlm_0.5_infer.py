c90a5159d88627a789c792714eefab814466d2b8
Srijan Das <srijan2001kgp@gmail.com>
2025-11-01 21:01:43 +0530

    Update vlm_0.5_infer.py

--- Codes/Inference/vlm/vlm_0.5_infer.py
+++ Codes/Inference/vlm/vlm_0.5_infer.py
@@ -12,6 +12,87 @@ from sklearn.metrics import f1_score,recall_score,precision_score
 import warnings
 warnings.filterwarnings('ignore')
 
+def cut_and_infer(prob,cut_v):
+    # Convert input to numpy array if it's a list
+    prob = np.array(prob)
+    req_intrvl=[]
+    idx_above_cut=[]
+    for i in range(len(prob)):
+        if prob[i] > cut_v:
+            idx_above_cut.append(i)
+
+    if not idx_above_cut: # Handle case where no values are above the cut_v
+        return []
+
+    req_indx=[idx_above_cut[0]]
+    for i in range(1,len(idx_above_cut)):
+        if idx_above_cut[i]-idx_above_cut[i-1] > 1:
+            req_indx.append(idx_above_cut[i-1])
+            req_indx.append(idx_above_cut[i])
+    req_indx.append(idx_above_cut[-1])
+    #print(req_indx)
+    req_intrvl=[(req_indx[i],req_indx[i+1]) for i in range(0,len(req_indx),2)]
+    #merging intervals with a gap of 10 or less
+    req_intrvl_merge=[]
+    i=0
+    while i <= len(req_intrvl)-2:
+        if req_intrvl[i+1][0] - req_intrvl[i][1] <=10:
+            req_intrvl_merge.append((req_intrvl[i][0],req_intrvl[i+1][1]))
+            i=i+2
+        elif req_intrvl[i][0] != req_intrvl[i][1]:  #avoiding adding (r,r) types of element
+            req_intrvl_merge.append(req_intrvl[i])
+            i=i+1
+        else:
+            i=i+1
+    if req_intrvl[-1][0] - req_intrvl[-2][1] >10 and req_intrvl[-1][0] != req_intrvl[-1][1]:
+        req_intrvl_merge.append(req_intrvl[-1])
+    elif req_intrvl[-1][0] - req_intrvl[-1][1]<=10:
+        req_intrvl_merge[-1]=(req_intrvl_merge[-1][0],req_intrvl[-1][1])
+
+    predicted_indices=[]
+    for i in range(len(req_intrvl_merge)):
+        s=req_intrvl_merge[i][0]
+        e=req_intrvl_merge[i][1]
+        predicted_indices.append(s+np.argmax(prob[s:e+1]))
+
+    return np.array(predicted_indices)
+    
+def squeeze_array(GT, PRD, w):
+    n = len(GT)
+    ones = np.where(GT == 1)[0]
+    if ones.size == 0:
+        return np.array([0], dtype=int)
+
+    intervals = [(max(0, i - w), min(n - 1, i + w)) for i in ones]
+    intervals.sort(key=lambda x: x[0])
+    modf_GT = []
+    modf_PRD=[]
+    if intervals[0][0] > 0:
+       [modf_GT.append(0) for _ in range(intervals[0][0])]
+       [modf_PRD.append(PRD[i]) for i in range(intervals[0][0])]
+    i=0
+    while i<len(intervals)-1:
+      modf_GT.append(1)
+      if sum(PRD[intervals[i][0]:intervals[i][1]+1])>0:
+        modf_PRD.append(1)
+      else:
+        modf_PRD.append(0)
+      s1, e1 = intervals[i]
+      s2, e2 = intervals[i+1]
+      i=i+1
+      if s2-e1-1>0:
+        [modf_GT.append(0) for _ in range(s2-e1-1)]
+        [modf_PRD.append(PRD[j]) for j in range(e1+1,s2)]
+    modf_GT.append(1)
+    if sum(PRD[intervals[-1][0]:intervals[-1][1]+1])>0:
+        modf_PRD.append(1)
+    else:
+        modf_PRD.append(0)
+    if intervals[-1][1] <= n - 1:
+      [modf_GT.append(0) for _ in range(n - intervals[-1][1] - 1)]
+      [modf_PRD.append(PRD[i]) for i in range(intervals[-1][1]+1,n)]
+    return np.array(modf_GT, dtype=int), np.array(modf_PRD, dtype=int)
+    
 class MultilabelVLMDataset(Dataset):
     """
     Dataset for multilabel classification with binary vectors
@@ -242,88 +323,6 @@ def custom_collate_fn(batch):
 
     return collated
 
-def squeeze_array(GT, PRD, w):
-    n = len(GT)
-    ones = np.where(GT == 1)[0]
-    if ones.size == 0:
-        return np.array([0], dtype=int)
-
-    intervals = [(max(0, i - w), min(n - 1, i + w)) for i in ones]
-    intervals.sort(key=lambda x: x[0])
-    modf_GT = []
-    modf_PRD=[]
-    if intervals[0][0] > 0:
-       [modf_GT.append(0) for _ in range(intervals[0][0])]
-       [modf_PRD.append(PRD[i]) for i in range(intervals[0][0])]
-    i=0
-    while i<len(intervals)-1:
-      modf_GT.append(1)
-      if sum(PRD[intervals[i][0]:intervals[i][1]+1])>0:
-        modf_PRD.append(1)
-      else:
-        modf_PRD.append(0)
-      s1, e1 = intervals[i]
-      s2, e2 = intervals[i+1]
-      i=i+1
-      if s2-e1-1>0:
-        [modf_GT.append(0) for _ in range(s2-e1-1)]
-        [modf_PRD.append(PRD[j]) for j in range(e1+1,s2)]
-    modf_GT.append(1)
-    if sum(PRD[intervals[-1][0]:intervals[-1][1]+1])>0:
-        modf_PRD.append(1)
-    else:
-        modf_PRD.append(0)
-    if intervals[-1][1] <= n - 1:
-      [modf_GT.append(0) for _ in range(n - intervals[-1][1] - 1)]
-      [modf_PRD.append(PRD[i]) for i in range(intervals[-1][1]+1,n)]
-    return np.array(modf_GT, dtype=int), np.array(modf_PRD, dtype=int)
-
-def cut_and_infer(prob,cut_v):
-    # Convert input to numpy array if it's a list
-    prob = np.array(prob)
-    req_intrvl=[]
-    idx_above_cut=[]
-    for i in range(len(prob)):
-        if prob[i] > cut_v:
-            idx_above_cut.append(i)
-
-    if not idx_above_cut: # Handle case where no values are above the cut_v
-        return []
-
-    req_indx=[idx_above_cut[0]]
-    for i in range(1,len(idx_above_cut)):
-        if idx_above_cut[i]-idx_above_cut[i-1] > 1:
-            req_indx.append(idx_above_cut[i-1])
-            req_indx.append(idx_above_cut[i])
-    req_indx.append(idx_above_cut[-1])
-    #print(req_indx)
-    req_intrvl=[(req_indx[i],req_indx[i+1]) for i in range(0,len(req_indx),2)]
-    #merging intervals with a gap of 10 or less
-    req_intrvl_merge=[]
-    i=0
-    while i <= len(req_intrvl)-2:
-        if req_intrvl[i+1][0] - req_intrvl[i][1] <=10:
-            req_intrvl_merge.append((req_intrvl[i][0],req_intrvl[i+1][1]))
-            i=i+2
-        elif req_intrvl[i][0] != req_intrvl[i][1]:  #avoiding adding (r,r) types of element
-            req_intrvl_merge.append(req_intrvl[i])
-            i=i+1
-        else:
-            i=i+1
-    if req_intrvl[-1][0] - req_intrvl[-2][1] >10 and req_intrvl[-1][0] != req_intrvl[-1][1]:
-        req_intrvl_merge.append(req_intrvl[-1])
-    elif req_intrvl[-1][0] - req_intrvl[-1][1]<=10:
-        req_intrvl_merge[-1]=(req_intrvl_merge[-1][0],req_intrvl[-1][1])
-
-    predicted_indices=[]
-    for i in range(len(req_intrvl_merge)):
-        s=req_intrvl_merge[i][0]
-        e=req_intrvl_merge[i][1]
-        predicted_indices.append(s+np.argmax(prob[s:e+1]))
-
-    return np.array(predicted_indices)
-
-
 # usage
 def main():
 
@@ -502,3 +501,4 @@ if __name__ == "__main__":
     main()
     print("---------Finished computation--------")
     gc.collect()
+
