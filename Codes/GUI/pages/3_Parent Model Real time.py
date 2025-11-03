6af57f88af15c8e6d47ac6d9c2e6cc3c7c6b5710
Srijan Das <srijan2001kgp@gmail.com>
2025-11-03 22:28:10 +0530

    Remove cut_and_infer and plot_image functions
    Removed the cut_and_infer function and commented-out plot_image function.

--- Codes/GUI/pages/3_Parent Model Real time.py
+++ Codes/GUI/pages/3_Parent Model Real time.py
@@ -11,56 +11,6 @@ Trainable parameters: 9,556,096\n\n
 Percentage trainable: 4.14%'''
             )
 
-def cut_and_infer(prob,cut_v):
-  # Convert input to numpy array if it's a list
-    prob = np.array(prob)
-
-    req_intrvl=[]
-    idx_above_cut=[]
-
-    for i in range(len(prob)):
-        if prob[i] > cut_v:
-            idx_above_cut.append(i)
-
-    if not idx_above_cut: # Handle case where no values are above the cut_v
-        return []
-
-    req_indx=[idx_above_cut[0]]
-    for i in range(1,len(idx_above_cut)):
-        if idx_above_cut[i]-idx_above_cut[i-1] > 1:
-            req_indx.append(idx_above_cut[i-1])
-            req_indx.append(idx_above_cut[i])
-    req_indx.append(idx_above_cut[-1])
-    #print(req_indx)
-
-    req_intrvl=[(req_indx[i],req_indx[i+1]) for i in range(0,len(req_indx),2)]
-    #merging intervals with a gap of 10 or less
-    req_intrvl_merge=[]
-    i=0
-    while i <= len(req_intrvl)-2:
-        if req_intrvl[i+1][0] - req_intrvl[i][1] <=10:
-            req_intrvl_merge.append((req_intrvl[i][0],req_intrvl[i+1][1]))
-            i=i+2
-        elif req_intrvl[i][0] != req_intrvl[i][1]:  #avoiding adding (r,r) types of element
-            req_intrvl_merge.append(req_intrvl[i])
-            i=i+1
-        else:
-            i=i+1
-    if len(req_intrvl)>1:
-        if req_intrvl[-1][0] - req_intrvl[-2][1] >10 and req_intrvl[-1][0] != req_intrvl[-1][1]:
-            req_intrvl_merge.append(req_intrvl[-1])
-        elif req_intrvl[-1][0] - req_intrvl[-1][1]<=10:
-            req_intrvl_merge[-1]=(req_intrvl_merge[-1][0],req_intrvl[-1][1])
-    if len(req_intrvl)==1:
-        req_intrvl_merge.append(req_intrvl[0])
-    predicted_indices=[]
-    for i in range(len(req_intrvl_merge)):
-        s=req_intrvl_merge[i][0]
-        e=req_intrvl_merge[i][1]
-        predicted_indices.append(s+np.argmax(prob[s:e+1]))
-
-    return np.array(predicted_indices)
-
 def plot_image(ind_p,ind_t,ts):
             a=np.arange(0,256)
             fig,ax=plt.subplots(2,1,figsize=(8,6))
@@ -73,34 +23,6 @@ def plot_image(ind_p,ind_t,ts):
                 ax[i].set_ylabel(f'Channel {i+1} values')
                 ax[i].legend()
             st.pyplot(fig)
-# def plot_image(probs,labels,ts):
-
-#     st.write(":orange[Predicted anomaly locations]")
-#     ind_p=cut_and_infer(probs,np.mean(probs))
-#     # st.write(ind_p)
-#     cols = st.columns(len(ind_p))
-#     for i, c in enumerate(cols):
-#         c.markdown(f"<div style='text-align:center; border:1px solid #ccc; padding:5px; border-radius:5px;'>{ind_p[i]}</div>", unsafe_allow_html=True)
-#     st.write(":blue[True anomaly locations]")
-#     ind_t=np.where(labels==1)[0]
-#     # st.write(ind_t)
-#     cols = st.columns(len(ind_t))
-#     for i, c in enumerate(cols):
-#         c.markdown(f"<div style='text-align:center; border:1px solid #ccc; padding:5px; border-radius:5px;'>{ind_t[i]}</div>", unsafe_allow_html=True)
-#     a=np.arange(0,256)
-#     st.header('Resulting plot')
-#     fig,ax=plt.subplots(2,1,figsize=(10,10))
-#     col=['red','blue']
-#     # #1E3A8A,#7F1D1D
-#     for i in range(2):
-#         ax[i].plot(ts[:,i],color=col[i],alpha=0.7)
-#         ax[i].scatter(a[ind_t],ts[ind_t,i],marker='*',color='magenta',alpha=1,label='true anomalies',s=80)
-#         ax[i].scatter(a[ind_p],ts[ind_p,i],marker='o',color='green',alpha=1,label='predicted anomalies',s=70)
-#         ax[i].set_xlabel('Time steps')
-#         ax[i].set_ylabel(f'Channel {i+1} values')
-#         ax[i].legend()
-#     st.pyplot(fig)    
-
 
 st.header('Model variants')  
 st.write("Choose between two models with:")
